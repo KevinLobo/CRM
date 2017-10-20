6b9fe3d08e92f4a9a3a94c29e18b00839762ff24
MiguelJack <miguelfenix16@gmail.com>
2017-10-20 16:54:00 -0600

    Test
    Archivo test

---
+++ CRM/Test.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CRM
+{
+
+    [TestFixture]
+    public class Test
+    {
+    }
+}
