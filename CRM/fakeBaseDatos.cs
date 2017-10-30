7f180306195e1b78c97a05132d29bc99270be169
LuisDanielCL <daniel.cordero.leonhardes@gmail.com>
2017-10-30 15:37:32 -0600

    Pruebas en persona

--- CRM/fakeBaseDatos.cs
+++ CRM/fakeBaseDatos.cs
@@ -1,11 +1,57 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 
 namespace CRM
 {
-    public class fakeBaseDatos
+    public class fakeBaseDatos : IBaseDatos
     {
+        private Boolean salidaAbrir;
+        private Boolean salidaCerrar;
+        private Boolean salidaQuery;
+        private Boolean error = false;
+
+
+        public fakeBaseDatos(Boolean pSalidaAbrir, Boolean pSalidaCerrar, Boolean pSalidaQuery,
+                            Boolean pSaldaResultados, Boolean pError)
+        {
+            salidaAbrir = pSalidaAbrir;
+            salidaCerrar = pSalidaCerrar;
+            salidaQuery = pSalidaQuery;
+            error = pError;
+        }
+
+        public Boolean Abrir()
+        {
+            return salidaAbrir;
+        }
+        public Boolean Cerrar()
+        {
+            return salidaCerrar;
+        }
+        public Boolean cargarQuery(String comando)
+        {
+            return salidaQuery;
+        }
+
+        public IDataReader getSalida()
+        {
+            DataTable table = new DataTable();
+            DataRow row = table.NewRow();
+            table.Columns.Add(new DataColumn("Salida"));
+            row["Salida"] = "OK";
+            table.Rows.Add(row);
+            DataTableReader reader = new DataTableReader(table);
+            if (!error)
+            {
+                return reader;
+            }
+            else
+            {
+                throw new System.Exception("Error al acceder a la base de datos.");
+            }
+        }
     }
 }
